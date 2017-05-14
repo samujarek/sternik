ab979e1d4fc7b442618bc0c4cb614f67031909ed
samujarek <jaroslaw.janek@gmail.com>
2017-05-14 12:45:18 +0200

    Changes

--- src/main/java/com/sternik/samujarek/entities/Bus.java
+++ src/main/java/com/sternik/samujarek/entities/Bus.java
@@ -3,41 +3,35 @@ package com.sternik.samujarek.entities;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.hibernate.validator.constraints.NotEmpty;
-
-@Entity
+//@Entity
 @XmlRootElement
 public class Bus {
-	@NotNull
-	@Id
+//	@NotNull
+//	@Id
 	private Long idBus;
 
-	@NotNull
-	@Size(min = 2, max = 30)
+//	@NotNull
+//	@Size(min = 2, max = 30)
 	private Long passengersCapacity;
 
-	@Size(min = 2, max = 30)
+//	@Size(min = 2, max = 30)
 	private String brandBus;
 
-	@NotEmpty
+//	@NotEmpty
 	private String brandVersionBus;
 
-	@NotNull
+//	@NotNull
 	private BigDecimal vehiclePrice;
 
-	@NotNull
+//	@NotNull
 	private Date purchaseDate;
 
-	@NotEmpty
+//	@NotEmpty
 	private String vehicleHistory;
 
-	@NotNull
+//	@NotNull
 	private Status status;
 
 	public static Bus produceBus(Long idBus, String vehicleHistory, Long passengersCapacity, String brandVersionBus,
